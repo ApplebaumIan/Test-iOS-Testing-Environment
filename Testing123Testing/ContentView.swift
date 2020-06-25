53c7c1e9a3473eb4198be3f65fd41f4df0e3b475
Ian Applebaum <Ian.tyler@electricbluefish.com>
2020-06-25 15:25:29 -0400

    Initial Commit

---
+++ Testing123Testing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testing123Testing
+//
+//  Created by Ian Applebaum on 6/25/20.
+//  Copyright © 2020 Ian Applebaum. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
